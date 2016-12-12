8d4c2d9cb1feb59b258b687453e39b41e36056f5
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-12-12 17:16:15 +0100

    petit ajout pour respecter les consignes

--- mission01/build.sh
+++ mission01/build.sh
@@ -1,2 +1,5 @@
 docker build -t hello/world:v42 .
-docker run --rm --name hello_world -p 8080:80 hello/world:v42 &
+docker run -d --name hello_world -p 8080:80 --hostname 42born2code hello/world:v42 
+docker exec -ti hello_world bash
+docker kill hello_world
+docker rm hello_world
