2e973137f0bfee8e9dd469624ae935b3cb9959e6
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-12-14 14:39:12 +0100

    2222 to 4222

--- mission02/build.sh
+++ mission02/build.sh
@@ -1,3 +1,3 @@
-cp ~/.ssh/id_rsa.pub .
+cp /Users/alegent/.ssh/id_rsa.pub .
 docker build -t ssh/keys:v42 .
-docker run -d --name ssh_keys -p 2222:42 --hostname 42born2code ssh/keys:v42
+docker run -d --name ssh_keys -p 4222:42 --hostname 42born2code ssh/keys:v42
