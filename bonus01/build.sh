3219a419e806dff0d54cf14acf6ca7a1342dd671
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-12-20 12:11:05 +0100

    Le dossier bonus01 ce trouve sur la branche mongo_pb

--- bonus01/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# CONFIGURE MONGO FOR SECURITY
-docker build -t mongo-secure mongo \
-	&& echo "docker running secure ;)" \
-	&& docker run -ti --rm -v `pwd`/scripts:/scripts --name mongo mongo-secure
-
-# BUILD ALL AND UP
-#docker-compose up --build
--- bonus01/scripts/add_app.js
+++
@@ -1,9 +0,0 @@
-db.createUser({
-	"user": "app",
-	"pwd": "app",
-	"roles": [{
-		"db": "app",
-		"role": "readWrite"
-	}]
-});
-db.shutdownServer({});
