51e5acb695592d07017e9f948c62cf3c20ac2b1a
Alexandre LEGENT <alegent@e2r9p11.42.fr>
2017-03-09 11:38:51 +0100

    J'ai mis des versions dans mon bonus00 histoire d'etre sur que la mise a jour d'une image ne fasse pas tous planter.

--- bonus01/build.sh
+++ bonus01/build.sh
@@ -1,9 +1,7 @@
 mkdir -p database
-docker run -d -v $(pwd)/database:/data/db --name mongo-unsecure mongo
+docker run --rm -d -v $(pwd)/database:/data/db --name mongo-unsecure mongo --auth
 sleep 10
 docker exec mongo-unsecure mongo admin --eval "db.createUser({'user': 'root', 'pwd': 'root', 'roles': ['root']});"
-docker exec mongo-unsecure mongo app --eval "db.createUser({'user': 'app', 'pwd': 'app', 'roles': ['readWrite']});"
-docker exec mongo-unsecure mongo admin --eval "db.shutdownServer();"
-docker kill mongo-unsecure
-docker rm mongo-unsecure
+docker exec mongo-unsecure mongo app -u root -p root --authenticationDatabase admin --eval "db.createUser({'user': 'app', 'pwd': 'app', 'roles': ['readWrite']});"
+docker exec mongo-unsecure mongod --shutdown
 docker-compose up --build
